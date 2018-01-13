b2da7d7d3044e2d79acb00e4f74b46c0ea37a746
Harry Y <harrywye@gmail.com>
2018-01-12 18:14:28 -0800

    Implemented 'hash tag redirect'

--- ngblog-v1/src/app/digests/monthly-digest/monthly-digest.component.ts
+++ ngblog-v1/src/app/digests/monthly-digest/monthly-digest.component.ts
@@ -93,22 +93,22 @@ export class MonthlyDigestComponent implements OnInit {
     if (isDL()) dl.log(`>>> pagePath = ${pagePath}`);
     this.pageLinkPrefix = pagePath;
 
-    // this.activatedRoute.fragment.subscribe(fragment => {
-    //   if (isDL()) dl.log(`>>> fragment = ${fragment}`);
-    //   if (fragment) {
-    //     // Treating the fragment as the redirect url path.
-    //     // let redirectPath = decodeURIComponent(fragment);
-    //     let redirectPath = fragment;
-    //     let segments = fragment.split('/');
-    //     Observable.timer(1).subscribe(i => {
-    //       this.router.navigate(segments, { replaceUrl: true }).then(suc => {
-    //         if (isDL()) dl.log(`Redirect navigate() suc = ${suc}; fragment-path = ${redirectPath}`);
-    //       }).catch(err => {
-    //         if (isDL()) dl.log(`Redirect navigate() err = ${err}}`);
-    //       });
-    //     });
-    //   }
-    // });
+    this.activatedRoute.fragment.subscribe(fragment => {
+      if (isDL()) dl.log(`>>> fragment = ${fragment}`);
+      if (fragment) {
+        // Treating the fragment as the redirect url path.
+        // let redirectPath = decodeURIComponent(fragment);
+        let redirectPath = fragment;
+        let segments = fragment.split('/');
+        Observable.timer(1).subscribe(i => {
+          this.router.navigate(segments, { replaceUrl: true }).then(suc => {
+            if (isDL()) dl.log(`Redirect navigate() suc = ${suc}; fragment-path = ${redirectPath}`);
+          }).catch(err => {
+            if (isDL()) dl.log(`Redirect navigate() err = ${err}}`);
+          });
+        });
+      }
+    });
     // Routing trick with hash tags
 
 
